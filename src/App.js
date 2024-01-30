b7829f453d08e31a1401e08ab2ed9f74f284fb33
Daniel Mori <venuswhispers@gamil.com>
2024-01-31 06:43:27 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import './App.css';
+import { Home } from "./views";
+
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from "@ethersproject/providers";
+// import GA from './utils/GoogleAnalytics'
+import { useAnalytics } from './utils/GoogleAnalytics'
+import { Wrapper } from './components/Wrapper'
+
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
+
+function App() {
+  
+  const { initialized } = useAnalytics()
+  return (
+    // <div>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <BrowserRouter>
+        
+          <Wrapper initialized={initialized} />
+          <Routes>
+            {/* { GA.init() && <GA.RouteTracker /> } */}
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Web3ReactProvider>
+      
+    // </div>
+  );
+}
+
+export default App;
